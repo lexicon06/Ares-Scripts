c78e9be164997dc5bf4b8b1213e45e1324824024
Pablo <pablo.hehe2@gmail.com>
2025-10-06 10:18:47 -0300

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,51 +1,78 @@
-langCodes = [
-  "ar",
-  "de",
-  "en",
-  "es",
-  "fr",
-  "it",
-  "ja",
-  "ko",
-  "nl",
-  "pl",
-  "pt",
-  "ru",
-  "sv",
-  "bg",
-  "tl",
-  "tr",
-  "pl",
+// ============================================================
+//  Auto-Translator v1.0
+//  Description: Detects messages starting with a language pair
+//  (e.g., "esen Hola") and translates them using Google Translate.
+//
+//  Example usage:
+//    esen Hola -> translates from Spanish (es) to English (en)
+//
+//  Dependencies: HttpRequest, stripColors, user(), print()
+//  Compatibility: ES5 / Ares Script Engine
+// ============================================================
+
+// Supported ISO language codes
+var langCodes = [
+  "ar", "de", "en", "es", "fr", "it",
+  "ja", "ko", "nl", "pl", "pt", "ru",
+  "sv", "bg", "tl", "tr", "pl"
 ];
 
-//function onCommand(userobj, command, tUser, arguments) {
-function onTextAfter(userobj, text){
+/**
+ * Called after a user sends a message.
+ * Detects translation prefixes like "esen" and initiates a translation request.
+ */
+function onTextAfter(userobj, text) {
+  var cleanText = stripColors(text).toLowerCase();
+
   for (var i = 0; i < langCodes.length; i++) {
     for (var j = 0; j < langCodes.length; j++) {
-      if (i == j) continue;
+      if (i == j) continue; // skip same-language pairs
 
-      if (
-        stripColors(text).toLowerCase().startsWith(langCodes[i] + langCodes[j]+" ")
-      ) {
-        var text = stripColors(text).substring(5);
+      var prefix = langCodes[i] + langCodes[j] + " ";
+      if (cleanText.indexOf(prefix) === 0) {
+        // Extract the message after the prefix
+        var message = stripColors(text).substring(5);
+
+        // Build Google Translate API request
         var translate = new HttpRequest();
-        var uri = `http://translate.googleapis.com/translate_a/single?client=gtx&sl=${langCodes[i]}&tl=${langCodes[j]}&dt=t&q=${encodeURIComponent(text)}`;
+        var uri = "http://translate.googleapis.com/translate_a/single" +
+          "?client=gtx&sl=" + langCodes[i] +
+          "&tl=" + langCodes[j] +
+          "&dt=t&q=" + encodeURIComponent(message);
+
         translate.src = uri;
-        userobj.translate = langCodes[i] + langCodes[j];
-        translate.method = 'get';
+        translate.method = "get";
         translate.utf = true;
         translate.oncomplete = translationHandler;
+
+        // Store translation pair for callback use
+        userobj.translate = langCodes[i] + langCodes[j];
         translate.download(userobj.id);
+
+        return; // stop after first match
       }
     }
   }
 }
 
-function translationHandler(e){
-    if(e){
-        var data = JSON.parse(this.page);
-        var outputTextTranslated = `\x0312G\x0304o\x0307o\x0312g\x0303l\x0304e\x0315\x20(${user(parseInt(this.arg)).translate})\x0301:\x20\x0314\x20${data[0][0][0]}`;
+/**
+ * Handles the HTTP response from Google Translate.
+ * Displays the translated text to the same room where it was sent.
+ */
+function translationHandler(e) {
+  if (!e) return;
 
-        print(user(parseInt(this.arg)).vroom, outputTextTranslated);
-    }
+  try {
+    var data = JSON.parse(this.page);
+    var targetUser = user(parseInt(this.arg));
+    var translation = data[0][0][0];
+
+    var output = "\x0312G\x0304o\x0307o\x0312g\x0303l\x0304e\x0315 (" +
+      targetUser.translate +
+      ")\x0301:\x20\x0314 " + translation;
+
+    print(targetUser.vroom, output);
+  } catch (err) {
+    print("Translation error: " + err);
+  }
 }
